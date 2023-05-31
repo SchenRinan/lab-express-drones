1de33cab03492d2040c6b27cd9462412e0dc44da
Rinan <jandrewschnell@gmail.com>
2023-05-31 11:22:20 +0200

    day 1 Iteration 1 finished

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,14 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const droneSchema = new Schema({
+  // TODO: write the schema
+    name: String,
+    propellers: Number, 
+    maxSpeed: Number,
+});
+
+const Drones = mongoose.model('Drones', droneSchema);
+
+module.exports = Drones;
